7bf03ffb5cec214542868b4f015c6e020f1966fc
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2021-08-29 17:00:23 +0100

    Auto compile and mdbook build (#1)

--- index.mjs
+++ index.mjs
@@ -1,47 +1,149 @@
+import assert from 'assert';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
-import assert from 'assert';
 
+const outDir = 'src';
+const recipeDir = 'recipes';
+
+// Required sections in the yml file
 const REQUIRED_KEYS = [
-    'name',
+    'category',
     'description',
     'ingredients',
-    'steps'
+    'steps',
+    'title',
 ];
 
+
+/**
+ * 
+ * @param {Object} unordered    Sort an object alphabetically by key
+ * @returns {Object}            The object sorted
+*/
+function sortObject(unordered) {
+    const ordered = Object.keys(unordered).sort().reduce(
+        (obj, key) => { 
+            obj[key] = unordered[key]; 
+            return obj;
+        }, 
+        {}
+    );
+    return ordered;
+}
+
+
 (async() => {
-    for (const recipe of await fs.promises.readdir("./recipes")) {
-        const basename = path.basename(recipe, path.extname(recipe));
-        const out = `${basename}.md`;
-        console.log(`Generating ${recipe} > ${out}`);
-        const data = yaml.load(await fs.promises.readFile(path.join("./recipes", recipe)));
-        const mdFile = fs.createWriteStream(out);
-        REQUIRED_KEYS.forEach(k => assert.ok(data[k]))  
-        mdFile.write(`${data.name}\n`);
-        mdFile.write(`---------------\n`)
+    let summary = {};
+
+    // Iterate over all recipes
+    for (const recipeName of await fs.promises.readdir(recipeDir)) {
+        // Get the name of the directory
+        const outFileName = path.join(outDir, `${recipeName}.md`);
+        const recipeFilePath = path.join(recipeDir, recipeName, 'recipe.yml');
+        // console.log(`Generating ${recipeName} > ${outFileName}`);
+        
+        // Read recipe file and open out file
+        const data = yaml.load(await fs.promises.readFile(recipeFilePath));
+        const mdFile = fs.createWriteStream(outFileName);
+
+        // Ensure required sections are in the yml file
+        REQUIRED_KEYS.forEach(key => assert.ok(data[key]));
+
+        // Save to summary Object
+        if (!(data.category in summary)) {
+            summary[data.category] = [];
+        }
+        summary[data.category].push({'name': recipeName, 'title': data.title});
+
+        // Symlink images to src directory
+        const files = fs.readdirSync(path.join(recipeDir, recipeName));
+        const images = [];
+        files.forEach(file => {
+            if (file.endsWith('.jpg')) {
+                const linkImageDir = path.resolve(path.join(outDir, 'images', recipeName));
+                const linkSrc = path.resolve(path.join(recipeDir, recipeName, file));
+                const linkDst = path.resolve(path.join(outDir, 'images', recipeName, file))
+
+                // Delete and recreate recipe image directory
+                try {
+                    fs.rmdirSync(linkImageDir, { recursive: true });
+                } catch(_) {};
+                fs.mkdirSync(linkImageDir);
+                
+                // Delete the symlink if it exists
+                try {                  
+                    fs.unlinkSync(linkDst);
+                } catch (_) {};
+
+                // Symlink the image from the recipe directory to src
+                fs.symlinkSync(linkSrc, linkDst);
+
+                images.push(file);
+            }
+        });
+
+        // Write markdown
+
+        // Recipe header and description
+        mdFile.write(`# ${data.title}\n\n`);
         mdFile.write(`${data.description}\n`);
-        for (const image of data.images || []) {
-            mdFile.write(`![${image}](images/${image})\n`);
-        }
-        mdFile.write(`\n## Ingredients\n`);
+        if (images.includes('header.jpg')) {
+        mdFile.write(`![${data.title} header image](images/${recipeName}/header.jpg)`)}
+
+        // Ingredients
+        mdFile.write(`\n## Ingredients\n\n`);
         for (const ingredient of data.ingredients) {
-            mdFile.write(`- ${ingredient}\n`);
+            mdFile.write(`- ${ingredient.amount} ${ingredient.unit} ${ingredient.name}\n`);
         }
-        mdFile.write(`\n## Steps\n`);
+
+        // Steps
+        mdFile.write(`\n## Steps\n\n`);
         for (const step of data.steps) {
             const time = step.time ? ` (${step.time})` : '';
-            mdFile.write(`1. ${step.step}${time}\n`);
+            mdFile.write(`1. ${step.step}${time}`);
+
+            if (step.image) {
+                mdFile.write(`  \n![Step image ${step.image.replace('.jpg', '')}](images/${recipeName}/${step.image})\n`)
+            } else {
+                mdFile.write('\n');
+            }
         }
-        mdFile.write(`1. Serve\n`);
-        mdFile.write(`\n## Sources\n`);
+
+        // Sources
+        mdFile.write(`\n## Sources\n\n`);
         for (const source of data.sources || []) {
-            mdFile.write(`- ${source}\n`);
+            mdFile.write(`- <${source}>\n`);
         }
 
         mdFile.close();
-    }   
-})().catch((ex) => {
-    console.log(ex);
+    }
+
+    // Order summary and each category alphabetically
+    summary = sortObject(summary);
+    Object.keys(summary).forEach(key => {
+        summary[key] = sortObject(summary[key]);
+    })
+
+    // Write SUMMARY.md
+    const summaryFilePath = path.join(outDir, 'SUMMARY.md');
+    const summaryFile = fs.createWriteStream(summaryFilePath);
+    summaryFile.write('- [Recipes](index.md)\n')
+
+    // For each category
+    Object.keys(summary).forEach(category => {
+        const cat = summary[category];
+        summaryFile.write(`\n# ${category}\n\n`)
+
+        // For each recipe in the category
+        Object.keys(cat).forEach(recipe => {
+            const res = cat[recipe];
+            summaryFile.write(`- [${res.title}](${res.name}.md)\n`)
+        });
+    });
+
+    summaryFile.close();
+})().catch((error) => {
+    console.log(error);
     process.exit(1);
 })
--- src/index.md
+++ src/index.md
@@ -1,7 +1,3 @@
 # Recipes
 
 A collection of recipes (for food) that I have used in the past.
-
-## Index
-
-- [Halfy's Biryani](halfy-biryani.md)
